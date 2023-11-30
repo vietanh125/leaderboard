3f856df9dcbff6f82b39e3920a709ee2fa53c8cf
vietanh125 <nguyenvietanh12597@gmail.com>
2023-11-30 21:28:56 +0100

    update chai package

--- fetch_data.py
+++ fetch_data.py
@@ -1,121 +1,71 @@
-import requests
-import time
-import datetime
-from tqdm import tqdm
+import gradio as gr
+import numpy as np
 import pandas as pd
-from chai_guanaco.feedback import _get_latest_feedback
-from chai_guanaco.login_cli import auto_authenticate
-from chai_guanaco.metrics import FeedbackMetrics, _add_overall_rank
+import time
 
 
+intervals = (
+    ('hour', 3600),    # 60 * 60
+    ('min', 60),
+    ('sec', 1),
+)
 
-LEADERBOARD_ENDPOINT = "/leaderboard"
-PUBLIC_LEADERBOARD_MINIMUM_FEEDBACK_COUNT = 150
-LEADERBOARD_DISPLAY_COLS = [
-    'developer_uid',
-    'submission_id',
-    'status',
-    'thumbs_up_ratio',
-    'user_writing_speed',
-    'repetition',
-    'total_feedback_count',
-    'overall_rank',
-]
+def display_time(seconds, granularity=2):
+    result = []
 
-def get_all_historical_submissions(developer_key):
-    headers = {"developer_key": developer_key}
-    url = 'https://guanaco-submitter.chai-research.com/leaderboard'
-    resp = requests.get(url, headers=headers)
-    assert resp.status_code == 200, resp.json()
-    return resp.json()
+    for name, count in intervals:
+        value = seconds // count
+        if value:
+            seconds -= value * count
+            if value == 1:
+                name = name.rstrip('s')
+            result.append("{} {}".format(int(value), name))
+    return ' '.join(result[:granularity])
+rename = {'thumbs_up_ratio': 'thumbs_up', 'is_custom_reward': 'custom_reward', 'user_writing_speed': 'writing_speed', 'safety_score':'safety', 'total_feedback_count': 'feedbacks'}
+def get_db_prize(records):
+    prize = ['🥇 $10000', '🥈 $5000', '🥉 $2000', '🎖️ $1000', '🎖️ $750'] + ['🏃‍♀️ $250']*15
+    df = pd.read_csv('db.csv')
+    df['overall_rank'] = df['overall_rank'].rank().astype(int)
+    df = df.drop_duplicates('developer_uid', keep='first').reset_index(drop=True)
+    df = df.drop(columns=['repetition', 'model_repo'])
+    df = df.rename(columns=rename)
+    if len(prize) > len(df):
+        prize = prize[:len(df)]
+    else:
+        prize = prize + [""] * (len(df) - len(prize))
+    df['Prize'] = prize
+    update = 'Last updated ' + display_time(time.time() - df.loc[0]['last_updated']) + ' ago.'
+    df = df.drop(['last_updated'], axis=1)
+    df = df.round(5)
+    return update, df
 
-def cache(func, regenerate=False):
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        return result
-    return wrapper
+def get_db(records):
+    df = pd.read_csv('db.csv')
+    df['overall_rank'] = df['overall_rank'].rank().astype(int)
+    df = df.drop(columns=['repetition', 'model_repo'])
+    df = df.rename(columns=rename)
+    update = 'Last updated ' + display_time(time.time() - df.loc[0]['last_updated']) + ' ago.'
+    df = df.drop(['last_updated'], axis=1)
+    df = df.round(5)
+    return update, df
 
-@auto_authenticate
-def get_feedback(submission_id: str, developer_key=None):
-    # submissions = get_all_historical_submissions(developer_key)
-    # is_deployed = _submission_is_deployed(submission_id, submissions)
-    # load_feedback = _get_latest_feedback if is_deployed else _get_cached_feedback
-    load_feedback = _get_latest_feedback
-    feedback = load_feedback(submission_id, developer_key)
-    return feedback
+page1 = gr.Interface(
+        fn=get_db_prize,
+        inputs=None,
+        wrap=True,
+        outputs=[gr.Text(label='History'), gr.DataFrame(label='💎 Leaderboard')],
+        interactive=False,
+        allow_flagging='never',
+    )
+page2 = gr.Interface(
+        fn=get_db,
+        inputs=None,
+        wrap=True,
+        allow_flagging='never',
+        outputs=[gr.Text(label='History'), gr.DataFrame(label='💎 Leaderboard')],
+        interactive=False
+    )
+demo = gr.TabbedInterface([page1, page2], ["💰 Prize Leaderboard", "ℹ️ Detailed Leaderboard"])
 
-@auto_authenticate
-def display_leaderboard(
-        developer_key=None,
-        regenerate=False,
-        detailed=False,
-        ):
-    df = cache(get_leaderboard, regenerate)(developer_key)
-    # _print_formatted_leaderboard(df, detailed)
-    return df
-
-
-@auto_authenticate
-def get_leaderboard(developer_key=None):
-    submission_data = get_all_historical_submissions(developer_key)
-    leaderboard = []
-    for submission_id, meta_data in tqdm(submission_data.items(), 'Getting Metrics'):
-        metrics = get_submission_metrics(submission_id, developer_key)
-        meta_data.update(metrics)
-        leaderboard.append({'submission_id': submission_id, **meta_data})
-    return pd.DataFrame(leaderboard)
-
-
-@auto_authenticate
-def get_submission_metrics(submission_id, developer_key):
-    feedback = get_feedback(submission_id, developer_key)
-    feedback_metrics = FeedbackMetrics(feedback.raw_data)
-    metrics = {}
-    if len(feedback_metrics.convo_metrics) > 0:
-        metrics = {
-            'mcl': feedback_metrics.mcl,
-            'thumbs_up_ratio': feedback_metrics.thumbs_up_ratio,
-            'thumbs_up_ratio_se': feedback_metrics.thumbs_up_ratio_se,
-            'repetition': feedback_metrics.repetition_score,
-            'total_feedback_count': feedback_metrics.total_feedback_count,
-            'user_writing_speed': feedback_metrics.user_writing_speed,
-        }
-    return metrics
-
-
-
-
-def _get_processed_leaderboard(df):
-    # maintain backwards compatibility with model_name field
-    df['model_name'] = df['model_name'].fillna(df['submission_id'])
-    df = _format_leaderboard_date(df)
-    df = _filter_submissions_with_few_feedback(df)
-    df = df.reset_index(drop=True)
-    df = _add_overall_rank(df)
-    return df
-
-
-def _format_leaderboard_date(df):
-    df['timestamp'] = df.apply(lambda x: datetime.fromisoformat(x['timestamp']), axis=1)
-    df['date'] = df['timestamp'].dt.date
-    df.drop(['timestamp'], axis=1, inplace=True)
-    return df
-
-def _filter_submissions_with_few_feedback(df):
-    filtered_df = df[df.total_feedback_count >= PUBLIC_LEADERBOARD_MINIMUM_FEEDBACK_COUNT]
-    return filtered_df
-
-
-while True:
-    try:
-        df = display_leaderboard(regenerate=True, detailed=False)
-        df = _filter_submissions_with_few_feedback(df)
-        df = _add_overall_rank(df)
-        df = df[LEADERBOARD_DISPLAY_COLS]
-        df['last_updated'] = time.time()
-        df.to_csv('db.csv', index=False)
-    except KeyboardInterrupt:
-        print('interrupted!')
-        break
-    except:
-        continue
+if __name__ == "__main__":
+    demo.launch()
