ecad93209fe6b2fb18bf74ba7bd3190a121bbe71
vietanh125 <nguyenvietanh12597@gmail.com>
2023-12-03 16:20:53 +0100

    fix file name

--- app.py
+++ app.py
@@ -1,60 +1,71 @@
+import gradio as gr
 import numpy as np
-from chai_guanaco.utils import *
-from chai_guanaco.metrics import _get_formatted_leaderboard, _get_processed_leaderboard, _pprint_leaderboard, get_leaderboard, _filter_submissions_with_few_feedback
+import pandas as pd
 import time
 
-LEADERBOARD_ENDPOINT = "/leaderboard"
-PUBLIC_LEADERBOARD_MINIMUM_FEEDBACK_COUNT = 150
-DEFAULT_MAX_WORKERS = 20
-LEADERBOARD_DISPLAY_COLS = [
-    'developer_uid',
-    'submission_id',
-    'model_repo',
-    'status',
-    'is_custom_reward',
-    'thumbs_up_ratio',
-    'user_writing_speed',
-    'repetition',
-    'safety_score',
-    'total_feedback_count',
-    'overall_rank',
-]
 
+intervals = (
+    ('hour', 3600),    # 60 * 60
+    ('min', 60),
+    ('sec', 1),
+)
 
-def display_leaderboard(
-        developer_key=None,
-        regenerate=False,
-        detailed=False,
-        max_workers=DEFAULT_MAX_WORKERS
-):
-    df = get_leaderboard(max_workers=max_workers, developer_key=developer_key)
-    df = _get_processed_leaderboard(df, detailed)
-    df = _get_formatted_leaderboard(df, detailed)
-    _pprint_leaderboard(df)
-    return df
+def display_time(seconds, granularity=2):
+    result = []
 
+    for name, count in intervals:
+        value = seconds // count
+        if value:
+            seconds -= value * count
+            if value == 1:
+                name = name.rstrip('s')
+            result.append("{} {}".format(int(value), name))
+    return ' '.join(result[:granularity])
+rename = {'thumbs_up_ratio': 'thumbs_up', 'is_custom_reward': 'custom_reward', 'user_writing_speed': 'writing_speed', 'safety_score':'safety', 'total_feedback_count': 'feedbacks'}
+def get_db_prize(records):
+    prize = ['🥇 $10000', '🥈 $5000', '🥉 $2000', '🎖️ $1000', '🎖️ $750'] + ['🏃‍♀️ $250']*15
+    df = pd.read_csv('db.csv')
+    df['overall_rank'] = df['overall_rank'].rank().astype(int)
+    df = df.drop_duplicates('developer_uid', keep='first').reset_index(drop=True)
+    df = df.drop(columns=['repetition', 'model_repo'])
+    df = df.rename(columns=rename)
+    if len(prize) > len(df):
+        prize = prize[:len(df)]
+    else:
+        prize = prize + [""] * (len(df) - len(prize))
+    df['Prize'] = prize
+    update = 'Last updated ' + display_time(time.time() - df.loc[0]['last_updated']) + ' ago.'
+    df = df.drop(['last_updated'], axis=1)
+    df = df.round(5)
+    return update, df
 
-def _add_overall_rank(df):
-    thumbs_up_rank = df['thumbs_up_ratio'].rank(ascending=False)
-    writing_speed_rank = df['user_writing_speed'].rank(ascending=True)
-    df['overall_score'] = np.mean([writing_speed_rank, thumbs_up_rank], axis=0)
-    df['overall_rank'] = df.overall_score
-    df = df.sort_values('overall_rank').reset_index(drop=True)
-    return df
+def get_db(records):
+    df = pd.read_csv('db.csv')
+    df['overall_rank'] = df['overall_rank'].rank().astype(int)
+    df = df.drop(columns=['repetition', 'model_repo'])
+    df = df.rename(columns=rename)
+    update = 'Last updated ' + display_time(time.time() - df.loc[0]['last_updated']) + ' ago.'
+    df = df.drop(['last_updated'], axis=1)
+    df = df.round(5)
+    return update, df
 
-while True:
-    try:
-        os.system('rm -rf  /root/.chai-guanaco/cache')
-        df = display_leaderboard(regenerate=True, detailed=True)
-        df = _filter_submissions_with_few_feedback(df)
-        df = _add_overall_rank(df)
-        df = df[LEADERBOARD_DISPLAY_COLS]
-        df['last_updated'] = time.time()
-        df.to_csv('db.csv', index=False)
+page1 = gr.Interface(
+        fn=get_db_prize,
+        inputs=None,
+        wrap=True,
+        outputs=[gr.Text(label='History'), gr.DataFrame(label='💎 Leaderboard')],
+        interactive=False,
+        allow_flagging='never',
+    )
+page2 = gr.Interface(
+        fn=get_db,
+        inputs=None,
+        wrap=True,
+        allow_flagging='never',
+        outputs=[gr.Text(label='History'), gr.DataFrame(label='💎 Leaderboard')],
+        interactive=False
+    )
+demo = gr.TabbedInterface([page1, page2], ["💰 Prize Leaderboard", "ℹ️ Detailed Leaderboard"])
 
-    except KeyboardInterrupt:
-        print('interrupted!')
-        break
-    except Exception as e:
-        print(e)
-        continue
+if __name__ == "__main__":
+    demo.launch()
